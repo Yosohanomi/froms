99d6a74213788cab93b16c78dcb94ec2fe5f2019
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2025-06-09 22:16:44 +0300

    +

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector('#open-modal-btn')
 const backdrop = document.querySelector('#backdrop')
 const closeBtn = document.querySelector('#close-modal-btn')
+const modalForm = document.querySelector('.modal__form')
+const modalInput= document.querySelector('js-input')
 btn.addEventListener('click', toggleModal)
 closeBtn.addEventListener('click', toggleModal)
 
@@ -9,3 +11,16 @@ function toggleModal() {
 
     // або add, але тоді не закриється, бо тогл додає якщо нема, видаляє якщо клас є
 }
+
+modalForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.dir(event.currentTarget.elements.value);
+})
+
+modalInput.addEventListener('focus', ()=> {
+    console.log('focus made');
+})
+
+modalInput.addEventListener('blur', ()=> {
+    console.log('blur made');
+})
